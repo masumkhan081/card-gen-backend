d1fb63294b9dc75183d2fc3ff2b5d573f8796df7
Masum Khan <masumkhan081@gmail.com>
2024-08-18 18:02:20 +0600

    commited

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const axios = require('axios');
 const PORT = 3000;
 app.use(express.json());
 const cors = require('cors');
+const scrape = require("./scrape");
+const puppeteer = require("puppeteer");
 // 
 const corsOptions = {
       origin: 'http://localhost:5173',
@@ -15,10 +17,12 @@ app.use(cors(corsOptions));
 // 
 
 app.get('/', async (req, res) => {
-      res.send("hello world !")
+
+      scrape();
+      res.send(
+            "hello - world "
+      )
 });
-
-
 
 // Start the server
 app.listen(PORT, () => {
