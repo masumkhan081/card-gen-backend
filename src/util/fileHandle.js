c9069bc16907b856a5b828a8780e804738a21e59
Masum Khan <masumkhan081@gmail.com>
2024-08-25 12:31:17 +0600

    image extensions added

--- src/util/fileHandle.js
+++ src/util/fileHandle.js
@@ -75,14 +75,14 @@ const uploadMultiple = multer({
 }).single("files", 5);
 
 function checkFileType(file, cb) {
-  const filetypes = /jpeg|jpg|png|gif/;
+  const filetypes = /jpeg|jpg|png|gif|webp|svg/;
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   const mimetype = filetypes.test(file.mimetype);
 
   if (mimetype && extname) {
     return cb(null, true);
   } else {
-    cb("Error: Images only! (jpeg, jpg, png, gif)");
+    cb("Error: Images only! (jpeg, jpg, png, gif, webp, svg)");
   }
 }
 
