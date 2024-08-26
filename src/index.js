ca53f42fca6893a1563c646bc594d7038f273ff7
Masum Khan <masumkhan081@gmail.com>
2024-08-26 22:37:32 +0600

    single player get added

--- src/index.js
+++ src/index.js
@@ -1,33 +1,32 @@
-
-
-
 ///* eslint-disable no-undef */
 const app = require("./app");
 const mongodbConnection = require("./config/mongodb");
-// 
+//
 
 async function bootstrap() {
-      const server = app.listen(process.env.PORT, async () => {
-            console.log(`Server running on port ${process.env.PORT}`);
-            await mongodbConnection();
+  //
+  const PORT = process.env.PORT || 5000;
+  const server = app.listen(process.env.PORT, async () => {
+    console.log(`Server running on port ${PORT}`);
+    await mongodbConnection();
+  });
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.log("Server closed");
       });
+    }
+    process.exit(1);
+  };
 
-      const exitHandler = () => {
-            if (server) {
-                  server.close(() => {
-                        console.log("Server closed");
-                  });
-            }
-            process.exit(1);
-      };
+  const unexpectedErrorHandler = (error) => {
+    console.log(error);
+    exitHandler();
+  };
 
-      const unexpectedErrorHandler = (error) => {
-            console.log(error);
-            exitHandler();
-      };
-
-      process.on("uncaughtException", unexpectedErrorHandler);
-      process.on("unhandledRejection", unexpectedErrorHandler);
+  process.on("uncaughtException", unexpectedErrorHandler);
+  process.on("unhandledRejection", unexpectedErrorHandler);
 }
 
 bootstrap();
