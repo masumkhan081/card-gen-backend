58c2204f0dcf3de05d5ac5de18be21780eda655f
Masum Khan <masumkhan081@gmail.com>
2024-08-27 16:02:46 +0600

    commited

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,14 +2,16 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const mongodbConnection = async () => {
-      try {
-            await mongoose.connect(process.env.DB_URL, {
-                  dbName: "footballCards",
-            });
-            console.log("Mongodb connected!");
-      } catch (error) {
-            console.log("Mongodb not connected!");
-      }
+  try {
+    const DB_URL =
+      "mongodb+srv://masumkhan:pddrgj3q@cluster0.wyotw.mongodb.net/";
+    await mongoose.connect(DB_URL, {
+      dbName: "footballCards",
+    });
+    console.log("Mongodb connected!");
+  } catch (error) {
+    console.log("Mongodb not connected!");
+  }
 };
 
 module.exports = mongodbConnection;
